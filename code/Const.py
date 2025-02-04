d4503fd4927d3f7ce668bd7b787dcb3dc25b46ea
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-04 10:37:00 -0300

    Inserting assets Creating Menu Inserting the cont file

---
+++ code/Const.py
@@ -0,0 +1,12 @@
+COLOR_BLUE = (0,0,255)
+COLOR_WHITE = (255, 255, 255)
+
+MENU_OPTION = ( 'NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'SCORE',
+                'EXIT'
+
+)
+
+WIN_WIDTH = 700
+WIN_HEIGHT = 480
