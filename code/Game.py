6b5cfcc276cfe1d414a24f68db5dddde05063680
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-04 16:25:44 -0300

    Creating Menu

--- code/Game.py
+++ code/Game.py
@@ -1,5 +1,21 @@
+import pygame
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Menu import Menu
+
+
 class Game:
     def __init__(self):
-        self.window = None
-    def run(self, ):
-        pass
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
+
+
+
