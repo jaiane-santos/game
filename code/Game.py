8012d2ddc25c224d3bd773161d6fe82b9f41dff7
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-04 10:37:00 -0300

    Adding main loop and Connecting classes

--- code/Game.py
+++ code/Game.py
@@ -1,5 +1,23 @@
+import pygame
+
+from code.Menu import Menu
+
+
 class Game:
     def __init__(self):
-        self.window = None
-    def run(self, ):
-        pass
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
+           # for event in pygame.event.get():
+            #    if event.type == pygame.QUIT:
+             #       pygame.quit()
+              #      quit()
+
+
