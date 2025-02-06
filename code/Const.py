ab393cb1831c5f52c3b71a9080af7665ff7778ca
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-06 13:45:16 -0300

    Creating menu interations Creating abstract class Creating Factory Creating Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -1,8 +1,17 @@
 COLOR_BLUE = (0,0,255)
 COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 125)
+
+
+ENTITY_SPEED = {
+    'Level1Bg0': 1,
+    'Level1Bg1': 2,
+    'Level1Bg2': 3,
+}
+
 
 MENU_OPTION = ( 'NEW GAME 1P',
-                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COOP',
                 'SCORE',
                 'EXIT'
 
--- code/Entity.py
+++ code/Entity.py
@@ -1,7 +1,18 @@
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surf = None
-        self.rect = None
+from abc import ABC, abstractmethod
+
+import pygame.image
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+
+
+class Entity(ABC):
+    def __init__(self, name:str, position: tuple):
+        self.name = name
+        self.surf = pygame.image.load('./asset/' + name + '.png')
+        self.surf = pygame.transform.scale(self.surf, (WIN_WIDTH, WIN_HEIGHT))
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
+
+    @abstractmethod
     def move(self, ):
         pass
