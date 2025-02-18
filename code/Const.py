a1b3bbef9c9b91992c6a43c20c8a6d6b802d628d
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-18 15:26:14 -0300

    implementation sqlite3 for score

--- code/Const.py
+++ code/Const.py
@@ -154,8 +154,25 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 
 SPAWN_TIME = 4000
 
+
 TIMEOUT_STEP = 100
-TIMEOUT_LEVEL = 40000
+TIMEOUT_LEVEL = 30000
 
 WIN_WIDTH = 700
 WIN_HEIGHT = 480
+
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 60),
+             'EnterName': (WIN_WIDTH / 2, 99),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 130),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
