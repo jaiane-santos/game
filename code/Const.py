304e99f0518f008a8b2190b36651eb3e593255f6
jaiane <jaiane.santos@tokiomarine.com.br>
2025-02-18 10:55:51 -0300

    level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -9,6 +9,8 @@ C_CYAN = (0,255,255)
 
 EVENT_ENEMY = pygame.USEREVENT + 1
 
+EVENT_TIMEOUT = pygame.USEREVENT + 2
+
 
 ENTITY_SPEED = {
     'Level1Bg0': 1,
@@ -17,6 +19,15 @@ ENTITY_SPEED = {
     'Level1Bg3': 4,
     'Level1Bg4': 5,
     'Level1Bg5': 6,
+    'Level2Bg0': 1,
+    'Level2Bg1': 2,
+    'Level2Bg2': 3,
+    'Level2Bg3': 4,
+    'Level2Bg4': 5,
+    'Level2Bg5': 6,
+    'Level2Bg6': 7,
+    'Level2Bg7': 8,
+    'Level2Bg8': 9,
     'Player1': 2,
     'Player1Shot': 3,
     'Player2': 2,
@@ -36,7 +47,16 @@ ENTITY_HEALTH = {
     'Level1Bg3': 999,
     'Level1Bg4': 999,
     'Level1Bg5': 999,
-    'Player1': 300,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Level2Bg5': 999,
+    'Level2Bg6': 999,
+    'Level2Bg7': 999,
+    'Level2Bg8': 999,
+    'Player1': 30,
     'Player1Shot': 1,
     'Player2': 300,
     'Player2Shot': 1,
@@ -55,6 +75,15 @@ ENTITY_DAMAGE = {
     'Level1Bg3': 0,
     'Level1Bg4': 0,
     'Level1Bg5': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
+    'Level2Bg8': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -74,6 +103,15 @@ ENTITY_SCORE = {
     'Level1Bg3': 0,
     'Level1Bg4': 0,
     'Level1Bg5': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
+    'Level2Bg8': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -95,8 +133,8 @@ ENTITY_SHOT_DELAY = {
 }
 
 
-MENU_OPTION = ( 'NEW GAME 1P',
-                'NEW GAME 2P - COOP',
+MENU_OPTION = ( 'NEW GAME 1P (DEMO)',
+                'NEW GAME 2P - COOP (DEMO)',
                 'SCORE',
                 'EXIT'
 
@@ -115,6 +153,9 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                    'Player2': pygame.K_LCTRL}
 
 SPAWN_TIME = 4000
+
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 40000
 
 WIN_WIDTH = 700
 WIN_HEIGHT = 480
--- code/Level.py
+++ code/Level.py
@@ -6,7 +6,8 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN
+from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN, EVENT_TIMEOUT, \
+    TIMEOUT_STEP, TIMEOUT_LEVEL
 from code.Enemy import Enemy
 from code.EntityFactory import EntityFactory
 from code.Entity import Entity
@@ -15,20 +16,31 @@ from code.Player import Player
 
 
 class Level:
-    def __init__(self, window, name, game_mode):
-        self.timeout = 20000
+    def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
+        self.timeout = TIMEOUT_LEVEL
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg'))
+        player = EntityFactory.get_entity('Player1')
+        player.score = player_score[0]
+        self.entity_list.append(player)
         if game_mode in [MENU_OPTION[1]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2'))
+            player = EntityFactory.get_entity('Player2')
+            player.score = player_score[1]
+            self.entity_list.append(player)
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
+
+    def show_defeat_screen(self):
+        self.window.fill((0, 0, 0))
+        self.level_text(40, "Game over!", (255, 0, 0), (WIN_HEIGHT // 3, WIN_HEIGHT // 2))
+        pygame.display.flip()
+        pygame.time.delay(2000)
 
 
-    def run(self):
+    def run(self, player_score: list[int]):
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
@@ -50,6 +62,25 @@ class Level:
                 if event.type == EVENT_ENEMY:
                    choice = random.choice(('Enemy1', 'Enemy2', 'Enemy3'))
                    self.entity_list.append(EntityFactory.get_entity(choice))
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                               player_score[1] = ent.score
+                        return True
+
+                found_player = False
+                for ent in self.entity_list:
+                    if isinstance(ent, Player):
+                        found_player = True
+
+                if not found_player:
+                    self.show_defeat_screen()
+                    return False
+
 
 
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', C_WHITE, (10, 5))
